8cf93d6fd0371b2025452d6e60728970a3e67f9c
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-06-27 10:38:40 +0200

    Output format for loki liveness check.

--- loki/liveness.sh
+++ loki/liveness.sh
@@ -1,18 +1,17 @@
+
+echo "Thor:"
 curl http://localhost:5000/health
 if [ $? -ne 0 ]; then
-  echo "Thor is not healthy"
   exit 1
-else
-  echo "Thor is healthy"
 fi
+echo  ""
 
+echo "Odin:"
 curl http://localhost:4000/health
 if [ $? -ne 0 ]; then
-  echo "Odin is not healthy"
   exit 1
-else
-  echo "Odin is healthy"
 fi
+echo  ""
 
 echo "Hefe is operational."
 exit 0
