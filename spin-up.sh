64a7453ab4336ec346c89318148a4d7a18dbfce5
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-08-26 09:16:54 +0200

    Hold Hefe-terminalen oppe etter man stopper Carla. Enda en terminal for SR.

--- spin-up.sh
+++ spin-up.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/bash
+#!/usr/bin/zsh
 
 # Hefe
-gnome-terminal --working-directory="$HEFE_ROOT" -- make
+gnome-terminal --working-directory="$HEFE_ROOT" -- zsh -c "make; exec zsh"
 
 # Carla
 gnome-terminal --working-directory="$CARLA_ROOT" 
 
-# Scenario Runner
+# Scenario Runner, running scenarios
+gnome-terminal --working-directory="$SCENARIO_RUNNER_ROOT"
+
+# Scenario Runner, running ego vehicle
 gnome-terminal --working-directory="$SCENARIO_RUNNER_ROOT"
