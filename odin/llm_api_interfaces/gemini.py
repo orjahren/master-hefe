d087a62df390ae4ff44a558effe6d6896fa766e4
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-10-30 20:26:47 +0100

    Måtte printe info om modellen som ble brukt til eksperimentene.

--- odin/llm_api_interfaces/gemini.py
+++ odin/llm_api_interfaces/gemini.py
@@ -3,6 +3,8 @@ import os
 
 from google import genai
 
+DEFAULT_GEMINI_MODEL = "gemini-2.5-flash"
+
 
 def get_api_key() -> str:
     api_key = os.getenv("GEMINI_API_KEY")
@@ -22,8 +24,24 @@ def api_is_up():
 def execute_gemini_model(model_name: str, prompt: str) -> str:
 
     response = client.models.generate_content(
-        model=model_name or "gemini-2.5-flash",
+        model=model_name or DEFAULT_GEMINI_MODEL,
         contents=prompt
     )
 
     return response.text
+
+
+def print_model_info(model_id: str):
+    try:
+        model_info = client.models.get(model=model_id)
+
+        print(f"Information for model: {model_id}")
+        print(model_info)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+
+    print_model_info(DEFAULT_GEMINI_MODEL)
